308f09998cc62ad2d5ffbc43397a2b20d1605e72
Loïc Berthelot <neywen@neywen.net>
2012-03-13 17:58:24 +0100

    fixed bug in index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ include("mobile/Mobile_Detect.php");
     $detect = new Mobile_Detect();
 
     if ($detect->isMobile())
-        header("Location:mobile/index.php);
+        header("Location:mobile/index.php");
     else
         header("Location:fr/index.php"); 
 
